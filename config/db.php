f129c067fd197be8a0d0562b611891798a0529f3
Elena Nikulina <elnikulina@yandex.ru>
2018-11-25 18:42:09 +0700

    HW2

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=gbyii2-10.local;dbname=gbyii2-10.local',
+    'username' => 'gbyii2_10_local',
+    'password' => 'zxsa627332',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
